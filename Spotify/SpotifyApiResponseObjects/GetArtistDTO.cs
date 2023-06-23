0a9715b8bbee04e4ff18130287bae3e2b17bd647
computer-mann <hpsn16@gmail.com>
2023-06-23 00:59:53 +0000

    from the gheto to mansion

--- Spotify/SpotifyApiResponseObjects/GetArtistDTO.cs
+++ Spotify/SpotifyApiResponseObjects/GetArtistDTO.cs
@@ -93,42 +93,5 @@ namespace Spotify.Models.SpotifyApiResponseObjects
             public long Width { get; set; }
         }
  
-
-    //public class GetArtistDTO
-    //{ 
-    //    public Artists artists { get; set; }
-    //}
-    //public class Artists
-    //{
-    //    public External_Urls external_urls { get; set; }
-    //    public Followers followers { get; set; }
-    //    public string[] genres { get; set; }
-    //    public string href { get; set; }
-    //    public string id { get; set; }
-    //    public Image[] images { get; set; }
-    //    public string name { get; set; }
-    //    public int popularity { get; set; }
-    //    public string type { get; set; }
-    //    public string uri { get; set; }
-    //}
-
-    //    public class External_Urls
-    //    {
-    //        public string spotify { get; set; }
-    //    }
-
-    //    public class Followers
-    //    {
-    //        public object href { get; set; }
-    //        public int total { get; set; }
-    //    }
-
-    //    public class Image
-    //    {
-    //        public int height { get; set; }
-    //        public string url { get; set; }
-    //        public int width { get; set; }
-    //    }
-
 }
 
