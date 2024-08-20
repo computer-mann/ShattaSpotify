17dcda82dac7f6a8a7519638806adffb0bbeee63
Prince Harry Nunoo <hpsn16@gmail.com>
2024-08-20 11:22:47 +0000

    wait up

--- Spotify/WebApi.Presentation.Spotify/CustomMiddlewares/ApplicationBuilderMidddlewareExtension.cs
+++ Spotify/WebApi.Presentation.Spotify/CustomMiddlewares/ApplicationBuilderMidddlewareExtension.cs
@@ -15,10 +15,10 @@ namespace Spotify.CustomMiddlewares
             provider.UseScheduler(schedulers =>
             {
                // schedulers.Schedule<RefreshAppTokenCoravelService>().Hourly().RunOnceAtStart();
-                schedulers.Schedule<WeeklyDbSongClearanceHostedService>().EverySeconds(14).RunOnceAtStart();
-                schedulers.Schedule<GoogleNotificationHostedService>().EverySeconds(13).RunOnceAtStart();
-                schedulers.Schedule<CheckNewReleasesHostedService>().EverySeconds(12).RunOnceAtStart();
-                schedulers.Schedule(() => Console.WriteLine("Every second of the app running.")).EveryTenSeconds().RunOnceAtStart();
+                //schedulers.Schedule<WeeklyDbSongClearanceHostedService>().EverySeconds(14).RunOnceAtStart();
+                //schedulers.Schedule<GoogleNotificationHostedService>().EverySeconds(13).RunOnceAtStart();
+                //schedulers.Schedule<CheckNewReleasesHostedService>().EverySeconds(12).RunOnceAtStart();
+                //schedulers.Schedule(() => Console.WriteLine("Every second of the app running.")).EveryTenSeconds().RunOnceAtStart();
 
             });
             return app;
