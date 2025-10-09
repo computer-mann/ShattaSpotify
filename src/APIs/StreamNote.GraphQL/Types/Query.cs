1e7da69b7c72575e40f269ae724c661e9b35b5c2
Prince Harry Nunoo <princeharry.nunoo@hubtel.com>
2025-10-09 17:54:29 +0000

    9.0 main

--- src/APIs/StreamNote.GraphQL/Types/Query.cs
+++ src/APIs/StreamNote.GraphQL/Types/Query.cs
@@ -5,5 +5,8 @@ namespace StreamNote.GraphQL.Types
     {
         public static Book GetBook()
             => new Book("C# in depth.", new Author("Jon Skeet"));
+
+        public static string SayHello(string name="World")
+            => $"Hello, {name}!";
     }
 }
