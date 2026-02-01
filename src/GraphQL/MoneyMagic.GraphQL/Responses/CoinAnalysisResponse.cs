263b168448e1a8a7f69750510e06332925b79b62
computer-mann <hpsn16@gmail.com>
2026-02-01 09:23:29 +0000

    GEM

---
+++ src/GraphQL/MoneyMagic.GraphQL/Responses/CoinAnalysisResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MoneyMagic.GraphQL.Responses
+{
+    public class CoinAnalysisResponse
+    {
+        public string FinishReason { get; set; }
+        public string PromptResponse { get; set; }
+    }
+}
